91164026dde7f93344ce41a5b45ada3275689bbc
ShawnZhai <55163447+ShawnZhai219@users.noreply.github.com>
2022-10-08 09:10:49 +0800

    small bug fix

--- A1/memcache_Shawn/memcache/app/db_operations/keys.py
+++ A1/memcache_Shawn/memcache/app/db_operations/keys.py
@@ -9,20 +9,20 @@ def get_least_recently_used_key_db():
 
 
 def get_image_size_db(key):
-    query = "SELECT image_size FROM cache_keys WHERE key_value = %d"
+    query = "SELECT image_size FROM cache_keys WHERE key_value = %s"
     app.cursor.execute(query, key)
     for image_size in app.cursor:
         return image_size
 
 
 def update_time_last_used_db(current_time, key):
-    query = "UPDATE cache_keys SET time_last_used = %d WHERE key_value = %d"
-    app.cursor.execute(query, current_time, key)
+    query = "UPDATE cache_keys SET time_last_used = %s WHERE key_value = %s"
+    app.cursor.execute(query, (current_time, key))
     app.cnx.commit()
 
 
 def delete_key_db(key):
-    query = "DELETE FROM cache_keys WHERE key_value = %d"
+    query = "DELETE FROM cache_keys WHERE key_value = %s"
     app.cursor.execute(query, key)
     app.cnx.commit()
 
