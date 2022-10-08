474bb02c5655fb5f948e749d5d4834c874da2f18
Haoyu Xu <i@halyul.dev>
2022-10-08 19:01:52 -0400

    feat(flask 1): make database query more abstruct

--- A1/server/libs/database.py
+++ A1/server/libs/database.py
@@ -2,11 +2,14 @@ import mysql.connector
 from server.config import Config
 
 CONFIG = Config().fetch()["database"]
+CONFIG_TABLE_NAME = CONFIG["table_names"]["config"]
+KEY_IMAGE_TABLE_NAME = CONFIG["table_names"]["key_image"]
+STATUS_TABLE_NAME = CONFIG["table_names"]["status"]
 
 TABLES = [
-        "CREATE TABLE IF NOT EXISTS `key_image` (`id` int NOT NULL AUTO_INCREMENT, `key` varchar(64) NOT NULL, `image` varchar(1024) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci",
-        "CREATE TABLE IF NOT EXISTS `config` (`id` int NOT NULL AUTO_INCREMENT, `key` varchar(64) NOT NULL, `value` varchar(64) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci",
-        "CREATE TABLE IF NOT EXISTS `status` (`id` int NOT NULL AUTO_INCREMENT, `num_item_in_cache` int NOT NULL, `used_size` int NOT NULL, `total_request_served` int NOT NULL, `total_hit` int NOT NULL, `miss_rate` float DEFAULT NULL, `hit_rate` float DEFAULT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci",
+        "CREATE TABLE IF NOT EXISTS `{}` (`id` int NOT NULL AUTO_INCREMENT, `key` varchar(64) NOT NULL, `image` varchar(1024) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci".format(KEY_IMAGE_TABLE_NAME),
+        "CREATE TABLE IF NOT EXISTS `{}` (`id` int NOT NULL AUTO_INCREMENT, `key` varchar(64) NOT NULL, `value` varchar(64) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci".format(CONFIG_TABLE_NAME),
+        "CREATE TABLE IF NOT EXISTS `{}` (`id` int NOT NULL AUTO_INCREMENT, `num_item_in_cache` int NOT NULL, `used_size` int NOT NULL, `total_request_served` int NOT NULL, `total_hit` int NOT NULL, `miss_rate` float DEFAULT NULL, `hit_rate` float DEFAULT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci".format(STATUS_TABLE_NAME),
     ]
 
 class Database:
@@ -39,26 +42,26 @@ class Database:
     
     def __create_default_config(self):
         if not self.get_config():
-            self.__execute("INSERT INTO `{table_name}` (`key`, `value`) VALUES (%s, %s)".format(table_name="config"), ("policy", "rr"))
-            self.__execute("INSERT INTO `{table_name}` (`key`, `value`) VALUES (%s, %s)".format(table_name="config"), ("capacity", "100"))
+            self.__execute("INSERT INTO `{table_name}` (`key`, `value`) VALUES (%s, %s)".format(table_name=CONFIG_TABLE_NAME), ("policy", "rr"))
+            self.__execute("INSERT INTO `{table_name}` (`key`, `value`) VALUES (%s, %s)".format(table_name=CONFIG_TABLE_NAME), ("capacity", "100"))
     
     def get_config(self):
-        return self.__fetch("SELECT `key`, `value` FROM config")
+        return self.__fetch("SELECT `key`, `value` FROM {}".format(CONFIG_TABLE_NAME))
 
     def set_config(self, key, value):
-        self.__execute("UPDATE `{table_name}` SET `value` = %s WHERE `key` = %s".format(table_name="config"), (value, str(key)))
+        self.__execute("UPDATE `{table_name}` SET `value` = %s WHERE `key` = %s".format(table_name=CONFIG_TABLE_NAME), (value, str(key)))
 
     def get_status(self):
-        return self.__fetch("SELECT * FROM {table_name}".format(table_name="status"))
+        return self.__fetch("SELECT * FROM {table_name}".format(table_name=STATUS_TABLE_NAME))
 
     def create_key_image_pair(self, key, image):
-        self.__execute("INSERT INTO `{table_name}` (`key`, `image`) VALUES (%s, %s)".format(table_name="key_image"), (key, image))
+        self.__execute("INSERT INTO `{table_name}` (`key`, `image`) VALUES (%s, %s)".format(table_name=KEY_IMAGE_TABLE_NAME), (key, image))
     
     def get_key_image_pair(self, key):
-        return self.__fetch_one("SELECT `image` FROM `{table_name}` WHERE `key` = %s".format(table_name="key_image"), (key,))
+        return self.__fetch_one("SELECT `image` FROM `{table_name}` WHERE `key` = %s".format(table_name=KEY_IMAGE_TABLE_NAME), (key,))
     
     def get_keys(self):
-        return self.__fetch("SELECT `key` FROM {table_name}".format(table_name="key_image"))
+        return self.__fetch("SELECT `key` FROM {table_name}".format(table_name=KEY_IMAGE_TABLE_NAME))
 
     def disconnect(self):
         self.connection.close()
