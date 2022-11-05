21059f778b4bd5b2fa44d878b6dd0e81d81d8a71
ShawnZhai <55163447+ShawnZhai219@users.noreply.github.com>
2022-11-06 04:28:42 +0800

    Add an endpoint to get resize pool configs

--- A2/A_2/manager_app/routes.py
+++ A2/A_2/manager_app/routes.py
@@ -2,19 +2,14 @@ import services
 from manager_app import webapp
 
 
-@webapp.route('/api/manager/node_num', methods=['GET'])
-def get_node_num():
-    return services.get_node_num()
+@webapp.route('/api/manager/poolsize', methods=['GET'])
+def get_pool_size():
+    return services.get_pool_size()
 
 
-@webapp.route('/api/manager/aggregate_stats', methods=['GET'])
-def get_aggregate_stats():
-    return
-
-
-@webapp.route('/api/manager/config', methods=['POST'])
-def set_cache_configurations():
-    return services.set_cache_configurations()
+@webapp.route('/api/manager/poolsize/config', methods=['GET'])
+def get_resize_pool_config():
+    return services.get_resize_pool_config()
 
 
 @webapp.route('/api/manager/poolsize/manual', methods=['POST'])
@@ -25,6 +20,16 @@ def change_pool_size_manual():
 @webapp.route('/api/manager/poolsize/automatic', methods=['POST'])
 def change_pool_size_auto():
     return services.set_auto_scaler_parameters()
+
+
+@webapp.route('/api/manager/aggregate_stats', methods=['GET'])
+def get_aggregate_stats():
+    return
+
+
+@webapp.route('/api/manager/cache/config', methods=['POST'])
+def set_cache_configurations():
+    return services.set_cache_configurations()
 
 
 @webapp.route('/api/manager/cache/clear', methods=['DELETE'])
--- A2/A_2/manager_app/services.py
+++ A2/A_2/manager_app/services.py
@@ -1,28 +1,28 @@
 import boto3
 import requests
-from flask import request
+from flask import request, jsonify
 
+import manager_app
 from manager_app.ec2_helper_functions import ec2_get_instance, ec2_create, ec2_destroy, ec2_destroy_all
 from manager_app.responses import success_response, failed_response
 from manager_app.s3_helper_functions import s3_list, s3_clear
 from server.config import Config
-from manager_app import data
 
 CONFIG = Config().fetch()
 AUTO_SCALAR_URL = "http://{host}:{port}".format(**CONFIG["auto_scalar"])
 CACHE_URL = "http://{host}:{port}".format(**CONFIG["cache"])
 
 
-def get_node_num():
+def get_pool_size():
     instances = ec2_get_instance()
     node_num = instances.size()
     return success_response(node_num)
 
 
 def get_30_min_data():
-    if data.size >= 30:
-        data.pop(0)
-    data.append(get_stats_from_db())
+    if manager_app.data_30_min.size >= 30:
+        manager_app.data_30_min.pop(0)
+    manager_app.data_30_min.append(get_stats_from_db())
 
 
 def set_cache_configurations():
@@ -35,15 +35,27 @@ def set_cache_configurations():
     return success_response(content)
 
 
+def get_resize_pool_config():
+    content = jsonify(
+        resize_pool_option=manager_app.resize_pool_option,
+        resize_pool_parameters=manager_app.resize_pool_parameters
+    )
+    return success_response(content)
+
+
 def change_pool_size_manual():
     change = request.form.get('change')
+    manager_app.resize_pool_option = 'manual'
+    manager_app.resize_pool_parameters = {}
     instances = ec2_get_instance()
+
     if change == 'increase':
         if instances.size() == 8:
             return failed_response(400, "The size of memcache pool has been reached to maximum")
         else:
             ec2_create()
             return success_response("Memcache pool size increases")
+
     elif change == 'decrease':
         if not instances:
             return failed_response(400, "The size of memcache pool has been reached to minimum")
@@ -51,6 +63,7 @@ def change_pool_size_manual():
             instance = instances[0]
             ec2_destroy(instance.id)
             return success_response("Memcache pool size decreases")
+
     else:
         return failed_response(400, "Parameter change can only be increase or decrease")
 
@@ -60,13 +73,16 @@ def set_auto_scaler_parameters():
     min_miss_rate_threshold = request.form.get('min_miss_rate_threshold')
     ratio_expand_pool = request.form.get('ratio_expand_pool')
     ratio_shrink_pool = request.form.get('ratio_shrink_pool')
+    parameters = {'max_miss_rate_threshold': max_miss_rate_threshold,
+                  'min_miss_rate_threshold': min_miss_rate_threshold,
+                  'ratio_expand_pool': ratio_expand_pool,
+                  'ratio_shrink_pool': ratio_shrink_pool,
+                  'auto_mode': 'True'}
+    manager_app.resize_pool_option = 'automatic'
+    manager_app.resize_pool_parameters = parameters
 
     response = requests.post(AUTO_SCALAR_URL + "/api/scaler/config",
-                             data={'max_miss_rate_threshold': max_miss_rate_threshold,
-                                   'min_miss_rate_threshold': min_miss_rate_threshold,
-                                   'ratio_expand_pool': ratio_expand_pool,
-                                   'ratio_shrink_pool': ratio_shrink_pool,
-                                   'auto_mode': 'True'})
+                             data=parameters)
     content = response["content"]
     return success_response(content)
 
