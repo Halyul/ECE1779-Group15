b2706abfb58e4748564f608b409dab30e485730f
ShawnZhai <55163447+ShawnZhai219@users.noreply.github.com>
2022-11-10 11:32:52 +0800

    Remove unused functions

--- A2/A_2/manager_app/helper_functions/ec2_helper.py
+++ A2/A_2/manager_app/helper_functions/ec2_helper.py
@@ -1,24 +1,6 @@
 import boto3
-import config
 
-
-# def ec2_get_instance():
-#     ec2 = boto3.resource('ec2')
-#     instances = ec2.instances.all()
-#     return instances
-
-
-def ec2_get_instance_ip(instance_id):
-    ec2 = boto3.resource('ec2')
-    instances = ec2.instances.filter(
-        Filters=[
-            {
-                'Name': 'instance-id',
-                'Values': [instance_id]
-            }
-        ])
-    instance = list(instances.all())[0]
-    return instance.public_ip_address
+from manager_app import config
 
 
 def ec2_create():
@@ -31,7 +13,3 @@ def ec2_create():
 def ec2_destroy(instance_id):
     ec2 = boto3.resource('ec2')
     ec2.instances.filter(InstanceIds=[instance_id]).terminate()
-
-# def ec2_destroy_all():
-#     ec2 = boto3.resource('ec2')
-#     ec2.instances.terminate()
