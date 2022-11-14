5a3d772543dc1c44debbd631d1df36203382a75e
Term-Service <timothy_shu_t@yahoo.com>
2022-11-14 00:19:49 -0500

    fix the issue of notify decrease may send out before nofity increase due to waiting for node running, now notify decrease will wait for node running as well

--- A2/A_2/auto_scaler/libs/scaler_operations.py
+++ A2/A_2/auto_scaler/libs/scaler_operations.py
@@ -13,7 +13,7 @@ import auto_scaler.statistics as statistics
 
 from auto_scaler.libs.scaler_support_func import gen_failed_responce, gen_success_responce, get_miss_rate, \
     get_cache_pool_size, remove_cache_node, add_cache_node, clear_cache_node, clear_all_cache_stats, \
-    notify_while_bring_up_node, check_if_node_is_up, refresh_node_list, set_node_list_from_node_list
+    notify_while_resize_pool, check_if_node_is_up, refresh_node_list, set_node_list_from_node_list
 from auto_scaler.libs.ec2_support_func import ec2_list, ec2_get_instance_ip
 
 def responce_main():
@@ -137,19 +137,14 @@ def check_miss_rate_every_min(manully_triggered = False):
                     else:
                         temtitive_pool_size -= 1
                         notify_info['action'] = 'delete'
-                        notify_info['ip'].append(ec2_get_instance_ip(config.cache_pool_ids[-1]))
+                        # notify_info['ip'].append(ec2_get_instance_ip(config.cache_pool_ids[-1 - len(notify_info['ip'])]))
+                        changed_id.append(config.cache_pool_ids[-1 - len(changed_id)])
                 if expected_pool_size != original_pool_size:
                     # clear cache num_GET_request_served and num_hit if pool size needs adjustment
                     clear_all_cache_stats()
-                    # TODO: need to match the API with A1
-                    if notify_info['action'] == 'delete':
-                        logging.info("check_miss_rate_every_min - deleting nodes, sending request to notify A1")
-                        logging.info("check_miss_rate_every_min - notify_info = {}".format(json.dumps(notify_info)))
-                        # response = requests.post('http://127.0.0.1:' + str(config.server_port) + '/api/notify', data=[('ip', notify_info['ip']), ('mode', 'automatic'), ('change', 'decrease')])
-                    else:
-                        # wait for nodes bring up, notify_while_bring_up_node will also notify A1 these new nodes
-                        thread = threading.Thread(target = notify_while_bring_up_node, args=(notify_info, changed_id,), daemon = True)
-                        thread.start()
+                    # wait for nodes bring up, notify_while_resize_pool will notify A1 these changed nodes
+                    thread = threading.Thread(target = notify_while_resize_pool, args=(notify_info, changed_id,), daemon = True)
+                    thread.start()
                         
             elif config.auto_mode == False:
                 # TODO: need to get the node_id list from manager
