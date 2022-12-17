d6ca4c1815b1a6915897be6a3d1ce2acf6fb53f1
ShawnZhai <55163447+ShawnZhai219@users.noreply.github.com>
2022-12-17 12:52:10 +0800

    Fix bug

--- A3/A_3/album/album/service.py
+++ A3/A_3/album/album/service.py
@@ -86,7 +86,7 @@ def share_image():
     if not flag:
         return False, 500, "Failed to retrieve the image."
 
-    content64 = "data:{};base64,".format(mimetypes.guess_type(file_name)[0]) + base64.b64encode(thumbnail).decode(
+    content64 = "data:{};base64,".format(mimetypes.guess_type(file_name)[0]) + base64.b64encode(content).decode(
         "utf-8")
     image_meta['content'] = content64
     return True, 200, dict(
@@ -131,7 +131,7 @@ def get_image_by_key():
         if not flag:
             return False, 500, "Failed to retrieve the image."
 
-        content64 = "data:{};base64,".format(mimetypes.guess_type(file_name)[0]) + base64.b64encode(thumbnail).decode("utf-8")
+        content64 = "data:{};base64,".format(mimetypes.guess_type(file_name)[0]) + base64.b64encode(content).decode("utf-8")
         image_meta['content'] = content64
         return True, 200, dict(
             image=[image_meta]
